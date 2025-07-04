47bb634f18a4632b637226ab2ea46c8f8acd07b5
Gergana Kraeva <122839261+GerganaKraeva@users.noreply.github.com>
2025-07-04 12:18:44 +0300

    Fetching Data with useEffect

--- gery-project/src/Home.js
+++ gery-project/src/Home.js
@@ -2,25 +2,22 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: "My new website", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, expedita doloribus neque enim optio esse alias quisquam distinctio, recusandae animi ducimus quis vero minus qui rem consectetur fuga nostrum similique!", author: "Petq", id: 1 },
-        { title: "Welcome party!", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, expedita doloribus neque enim optio esse alias quisquam distinctio, recusandae animi ducimus quis vero minus qui rem consectetur fuga nostrum similique!", author: "Ivo", id: 2 },
-        { title: "Web dev top tips", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, expedita doloribus neque enim optio esse alias quisquam distinctio, recusandae animi ducimus quis vero minus qui rem consectetur fuga nostrum similique!", author: "Petq", id: 3 }
-    ]);
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect( () => {
-        console.log("use effect ran");
-        console.log(blogs);
-    });
+       fetch("http://localhost:8000/blogs")
+       .then(res => {
+        return res.json();
+       })
+       .then( data => {
+        console.log(data);
+        setBlogs(data);
+       });
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs!"  handleDelete={handleDelete}/>
+            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Petq")} title="Petq`s blocks!" /> */}
 
         </div>
