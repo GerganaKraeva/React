970515f2abf1b11aa82f739087177d011c56451a
Gergana Kraeva <122839261+GerganaKraeva@users.noreply.github.com>
2025-07-06 02:08:23 +0300

    Add Create.js component

--- gery-project/src/App.js
+++ gery-project/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />}>
-            </Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
           </Routes>
         </div>
       </div>
