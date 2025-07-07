1a2535fc593889f1587f519c731a297bd1a10e20
Gergana Kraeva <122839261+GerganaKraeva@users.noreply.github.com>
2025-07-07 09:16:06 +0300

    added form and submit event

--- gery-project/src/Create.js
+++ gery-project/src/Create.js
@@ -1,9 +1,46 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Petq");
+
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        console.log(blog);
+        
+    }
+
+    return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={handleSumbit} >
+                <label>Block title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                ></input>
+                <label>Block body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Petq">Petq</option>
+                    <option value="Ivo">Ivo</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
